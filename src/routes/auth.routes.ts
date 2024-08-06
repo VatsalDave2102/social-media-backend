4b269d6dbec24546ee97b5f64d3ed397926f2f31
Ritesh Adwani <ritesh.t@simformsolutions.com>
2024-08-06 12:04:48 +0530

    feat: sm-33 - create api for refresh token and update verifyToken middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import validateRequest from '../middlewares/validateRequest';
-import { login, logout, register } from '../controllers/auth.controllers';
+import { login, logout, refreshToken, register } from '../controllers/auth.controllers';
 import { userLoginSchema, userRegistrationSchema } from '../schemas/auth.schemas';
 import upload from '../middlewares/multer';
 import verifyToken from '../middlewares/verifyToken';
@@ -17,5 +17,7 @@ authRouter.post(
 authRouter.post('/login', validateRequest(userLoginSchema), login);
 
 authRouter.post('/logout', verifyToken('accessToken'), logout);
+
+authRouter.post('/refresh-token', verifyToken('refreshToken'), refreshToken);
 
 export default authRouter;
