20fa9ba4bcf9d9b78f78823fe7104d43766be7b2
Ritesh Adwani <ritesh.t@simformsolutions.com>
2024-08-21 12:20:47 +0530

    refactor: sm-54 - refactor zod schema for auth

--- src/schemas/auth.schemas.ts
+++ src/schemas/auth.schemas.ts
@@ -1,31 +1,34 @@
 import * as z from 'zod';
 
-export const userRegistrationSchema = z
-  .object({
-    email: z.string().email({ message: 'Invalid email address' }),
-    password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
-    confirmPassword: z.string(),
-    name: z.string().min(2, { message: 'Name must be at least 1 character long' }),
-    bio: z.string().min(1, { message: 'Bio must be at least 1 character long' }).optional(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ['confirmPassword'],
-  });
-
-export const userLoginSchema = z.object({
+const authSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
   password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
+  confirmPassword: z.string(),
+  name: z.string().min(2, { message: 'Name must be at least 2 characters long' }),
+  bio: z.string().min(1, { message: 'Bio must be at least 1 character long' }).optional(),
 });
 
-export const forgotPasswordSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
+export const userRegistrationSchema = authSchema.refine(
+  (data) => data.password === data.confirmPassword,
+  {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  },
+);
+
+export const userLoginSchema = authSchema.pick({
+  email: true,
+  password: true,
 });
 
-export const resetPasswordSchema = z
-  .object({
-    password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
-    confirmPassword: z.string(),
+export const forgotPasswordSchema = authSchema.pick({
+  email: true,
+});
+
+export const resetPasswordSchema = authSchema
+  .pick({
+    password: true,
+    confirmPassword: true,
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
